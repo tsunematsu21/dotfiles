e3aa2e6b9ef6c31a0168db0ef7a8c67f7e858752
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2026-06-28 17:33:44 +0900

    add lsp buffer-local keymaps on attach

--- nvim/.config/nvim/plugin/40_lsp.lua
+++ nvim/.config/nvim/plugin/40_lsp.lua
@@ -1,5 +1,22 @@
 local later, now_if_args = Config.later, Config.now_if_args
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user-lsp-keymaps', { clear = true }),
+  callback = function(event)
+    local map = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = event.buf, desc = desc })
+    end
+
+    map('n', 'K', vim.lsp.buf.hover, 'LSP hover')
+    map('n', 'gr', vim.lsp.buf.references, 'LSP references')
+    map('n', '<leader>lr', vim.lsp.buf.rename, 'Rename symbol')
+    map({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, 'Code action')
+    map('n', '<leader>lf', function()
+      vim.lsp.buf.format({ async = true })
+    end, 'Format buffer')
+  end,
+})
+
 later(function()
   -- Useful status updates for LSP.
   vim.pack.add({ 'https://github.com/j-hui/fidget.nvim' })
