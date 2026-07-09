8d4ce35d18e213456dd7517867974f0edb4474f3
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2026-07-09 10:21:01 +0900

    refactor(nix): simplify host configuration wiring

--- nix/home.nix
+++ nix/home.nix
@@ -7,26 +7,14 @@
   ...
 }:
 let
-  dotfilesPath = "${config.home.homeDirectory}/dotfiles";
+  dotfilesPath = "${homeDirectory}/dotfiles";
   mkLink = path: config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/${path}";
 in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = username;
   home.homeDirectory = homeDirectory;
+  home.stateVersion = "26.05";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "26.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     # Shell and terminal tools
     fd
@@ -63,8 +51,6 @@ in
     herdr
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".zprofile".source = mkLink "config/zsh/.zprofile";
     ".zshrc".source = mkLink "config/zsh/.zshrc";
@@ -93,26 +79,5 @@ in
     '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/masato.tsunematsu/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
--- nix/host/mac.nix
+++ nix/host/mac.nix
@@ -1,39 +1,5 @@
-{
-  inputs,
-  self,
-  system,
-  ...
-}:
-
-let
+rec {
   username = "masato.tsunematsu";
   homeDirectory = "/Users/${username}";
-in
-inputs.nix-darwin.lib.darwinSystem {
-  inherit system;
-  specialArgs = {
-    inherit self system username;
-    nix-homebrew = inputs.nix-homebrew;
-  };
-  modules = [
-    ../darwin.nix
-    inputs.home-manager.darwinModules.home-manager
-    inputs.nix-homebrew.darwinModules.nix-homebrew
-    {
-      nixpkgs.overlays = [
-        inputs.llm-agents.overlays.default
-        (final: _prev: {
-          czg = final.callPackage ../packages/czg.nix { };
-          safe-chain = final.callPackage ../packages/safe-chain.nix { };
-        })
-      ];
-      users.users.${username}.home = homeDirectory;
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = { inherit inputs username homeDirectory; };
-        users.${username} = import ../home.nix;
-      };
-    }
-  ];
+  system = "aarch64-darwin";
 }
