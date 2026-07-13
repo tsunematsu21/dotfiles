d9349af918eef5a2537899a9d8816c0303aff961
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2026-07-13 21:44:39 +0900

    feat(nvim): :sparkles: improve diagnostic display

--- config/nvim/plugin/40_lsp.lua
+++ config/nvim/plugin/40_lsp.lua
@@ -20,4 +20,23 @@ now_if_args(function()
       'typos_lsp',
     }
   }
+
+  vim.diagnostic.config {
+    severity_sort = true,
+    virtual_text = {
+      format = function(diag)
+        local details = diag.source
+        if diag.code ~= nil then
+          local code = tostring(diag.code)
+          details = details and ('%s: %s'):format(details, code) or code
+        end
+
+        return details and ('%s (%s)'):format(diag.message, details) or diag.message
+      end,
+    },
+    float = {
+      border = 'rounded',
+      source = 'if_many',
+    },
+  }
 end)
