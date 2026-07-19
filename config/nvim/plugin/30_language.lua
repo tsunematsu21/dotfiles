1a407c4c16c04dfcc7cbc900c21fa8950eb95f18
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2026-07-19 19:06:53 +0900

    refactor(nvim): :recycle: migrate to tree-sitter-manager
    Replace nvim-treesitter parser installation and highlighting with tree-sitter-manager. Remove the unused nvim-treesitter-textobjects dependency.

--- config/nvim/plugin/30_language.lua
+++ config/nvim/plugin/30_language.lua
@@ -2,8 +2,7 @@ local now_if_args = Config.now_if_args
 
 -- Tree-sitter
 vim.pack.add({
-  "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+  "https://github.com/romus204/tree-sitter-manager.nvim",
 })
 
 local languages = {
@@ -17,29 +16,16 @@ local languages = {
   "markdown",
 }
 
-local function is_missing(language)
-  local parser = ("parser/%s.*"):format(language)
-  return #vim.api.nvim_get_runtime_file(parser, false) == 0
-end
-
-local missing = vim.tbl_filter(is_missing, languages)
-if #missing > 0 then
-  require("nvim-treesitter").install(missing)
-end
-
-local filetypes = {}
-for _, language in ipairs(languages) do
-  vim.list_extend(filetypes, vim.treesitter.language.get_filetypes(language))
-end
-
-local group = vim.api.nvim_create_augroup("treesitter", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = group,
-  pattern = filetypes,
-  callback = function(event)
-    vim.treesitter.start(event.buf)
-  end,
-  desc = "Start Tree-sitter",
+require("tree-sitter-manager").setup({
+  assume_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+  },
+  ensure_installed = languages,
+  highlight = languages,
 })
 
 -- Fold
